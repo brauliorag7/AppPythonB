c88b351edf9f5887702481260d2b45630a5d6fd4
Ulyses <ulyses.rico.2012@gmail.com>
2020-11-10 21:37:22 -0600

    ...

--- utils.py
+++ utils.py
@@ -83,9 +83,11 @@ def imprimirEstadistica():
 
 def chartDecTree1(clf,feature_cols,target_cols):
     print('Generando gráfica')   
-    dot_data=export_graphviz(clf, out_file=None,feature_names = feature_cols)
-    graph = pydotplus.graph_from_dot_data(dot_data) 
-    Image(graph.create_png()) #Error aqui GraphViz's executables not found
+    dot_data = StringIO()
+    export_graphviz(clf, out_file=dot_data,  filled=True, rounded=True,special_characters=True, feature_names = feature_cols)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+    graph.write_png('clf.png')
+    Image(graph.create_png())
 
 
 
